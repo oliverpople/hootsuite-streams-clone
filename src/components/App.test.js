312dbd1f3b96c11b0a7f86166e4f5ccd814802f0
oliverpople <oliverpople@gmail.com>
2018-02-08 11:38:22 +0000

    Reafactored tests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,24 +2,36 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
+describe('App', () => {
 const app = shallow(<App />);
 
-it('renders correctly', () => {
-  expect(app).toMatchSnapshot();
+  it('renders correctly', () => {
+    expect(app).toMatchSnapshot();
+  });
+
+  it('initialises the `state` with an empty list of streams', () => {
+    expect(app.state().streams).toEqual([]);
+  });
+
+
+  describe('when clicking `add-stream` button', () => {
+
+    beforeEach(() => {
+      app.find('.btn-add').simulate('click');
+    });
+
+    afterEach(() => {
+      app.setState({ streams: [] });
+    });
+
+    it('adds a new stream to `state` when clicking `add stream` button', () => {
+      expect(app.state().streams).toEqual([{ id: 1 }]);
+    });
+
+    it('adds a new stream to the rendered list when clicking the `add stream` button', () => {
+      expect(app.find('.stream-list').children().length).toEqual(1);
+    });
+
+  });
+
 });
-
-it('initialises the `state` with an empty list of streams', () => {
-  expect(app.state().streams).toEqual([]);
-})
-
-it('adds a new stream to `state` when clicking `add stream` button', () => {
-  app.find('.btn-add').simulate('click');
-
-  expect(app.state().streams).toEqual([{ id: 1 }]);
-});
-
-it('adds a new stream to the rendered list when clicking the `add stream` button', () => {
-  app.find('.btn-add').simulate('click');
-
-  expect(app.find('.stream-list').children().length).toEqual(2);
-})
