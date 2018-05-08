5db60babf562b3985654d5007adb89d2eb9ae2f9
oliverpople <oliverpople@gmail.com>
2018-05-08 08:23:29 -0700

    Tweets now display user`s profile image, or if a retweet, the profile image of the retweeted user. All test pass, including test to see whether StreamItem renders an image

--- react-client/src/components/StreamItem.js
+++ react-client/src/components/StreamItem.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 
 class StreamItem extends Component {
 
+  tweetOrRetweetProfileImageSetter = tweet => {
+
+    const tweetProfileImage = tweet.hasOwnProperty('retweeted_status') ? tweet.retweeted_status.user.profile_image_url : tweet.user.profile_image_url;
+    return tweetProfileImage
+
+  }
+
   render() {
       return (
         <ul>
-          {this.props.streamItemsFromParent.map(tweet => <li key={tweet.id}>{tweet.text}</li>)}
+          {this.props.streamItemsFromParent.map(tweet =>
+            <li key={tweet.id}><img src={this.tweetOrRetweetProfileImageSetter(tweet)} alt='Profile' />{tweet.text}</li>
+          )}
         </ul>
       )
   }
 }
+
 
 export default StreamItem;
--- react-client/src/components/StreamItem.test.js
+++ react-client/src/components/StreamItem.test.js
@@ -3,15 +3,20 @@ import { shallow } from 'enzyme';
 import StreamItem from './StreamItem';
 
 const fs = require("fs");
-const mockData = fs.readFileSync('../express-backend/mock_data/tweet.json');
+const mockData = JSON.parse(fs.readFileSync('../express-backend/mock_data/tweet.json', 'utf8'));
 
 
 describe('StreamItem', () => {
+  const mockRemove = jest.fn();
   const props = {streamItemsFromParent: mockData}
   const streamItem = shallow(<StreamItem {...props} />);
 
   it('renders properly', () => {
-    expect(streamItem.toMatchSnapshot);
+    expect(streamItem).toMatchSnapshot();
+  });
+
+  it('renders the tweet`s text', () => {
+    expect(streamItem.find('img').length).toEqual(1);
   });
 
 });
